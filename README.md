5217f3475fe2e5ca19f8b4818262bb95bd688a07
Jeigsaw <sumanull@outlook.com>
2022-08-21 18:27:35 +0545

    updated to be published as npm package

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Skewuy
 Add 3D rotation feel without the need of rendering the whole 3D object on your webpage.
-Demo at :
+Demo at : https://jeigsaw.github.io/Skewuy/
+
+
--- dev/test site/main.js
+++ dev/test site/main.js
@@ -1,12 +1,39 @@
 // window load
+import skewuy from './../skewuy.js';
+
 window.onload = function (){
     const parent = document.querySelector("#skewuyContainer");
-    const skewuy1 = new Skewuy(200, 12,86,427,"../../docs/assets/spriteer.png",parent);
+    skewuy({
+        viewHeight: 200,
+        frameCount: 12, 
+        frameGutter: 86, 
+        frameWidth: 427, 
+        srcImg: "../../docs/assets/spriteer.png", 
+        skewuyContainer: parent
+    })
+
+    // const skewuy1 = new Skewuy(200, 12,86,427,"../../docs/assets/spriteer.png",parent);
 
     const parent2 = document.querySelector("#skewuyContainer2");
-    const skewuy2 = new Skewuy(200, 15,86,427,"../../docs/assets/cup_sprite.png",parent2);
+    skewuy({
+        viewHeight: 200,
+        frameCount: 15, 
+        frameGutter: 86, 
+        frameWidth: 427, 
+        srcImg: "../../docs/assets/cup_sprite.png", 
+        skewuyContainer: parent2
+    })
+    // const skewuy2 = new Skewuy(200, 15,86,427,"../../docs/assets/cup_sprite.png",parent2);
 
     const parent3 = document.querySelector("#skewuyContainer3");
-    const skewuy3 = new Skewuy(200, 12,86,427,"../../docs/assets/cuptwelve.png",parent3);
+    skewuy({
+        viewHeight: 200,
+        frameCount: 12, 
+        frameGutter: 86, 
+        frameWidth: 427, 
+        srcImg: "../../docs/assets/cuptwelve.png", 
+        skewuyContainer: parent3
+    })
+    // const skewuy3 = new Skewuy(200, 12,86,427,"../../docs/assets/cuptwelve.png",parent3);
 
 }
