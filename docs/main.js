bc763de64097a9d57614a1cec0753732c6267128
Jeigsaw <sumanull@outlook.com>
2022-08-21 20:19:08 +0545

    npm install test on docs

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,38 @@
+import skewuy from 'skewuy';
+
+
 window.onload = function (){
     const parent = document.querySelector("#skewuyContainer");
-    const skewuy1 = new Skewuy(200, 12,86,427,"./assets/sneaker.png",parent);
+    skewuy({
+        viewHeight: 200,
+        frameCount: 12, 
+        frameGutter: 86, 
+        frameWidth: 427, 
+        srcImg: "./assets/sneaker.png", 
+        skewuyContainer: parent
+    })
+    // const skewuy1 = new Skewuy(200, 12,86,427,"./assets/sneaker.png",parent);
 
     const parent2 = document.querySelector("#skewuyContainer2");
-    const skewuy2 = new Skewuy(200, 15,86,427,"./assets/cup.png",parent2);
+    skewuy({
+        viewHeight: 200,
+        frameCount: 15, 
+        frameGutter: 86, 
+        frameWidth: 427, 
+        srcImg: "./assets/cup.png", 
+        skewuyContainer: parent2
+    })
+    // const skewuy2 = new Skewuy(200, 15,86,427,"./assets/cup.png",parent2);
 
     const parent3 = document.querySelector("#skewuyContainer3");
-    const skewuy3 = new Skewuy(200, 15,86,427,"./assets/toyota.png",parent3);
+    skewuy({
+        viewHeight: 200,
+        frameCount: 15, 
+        frameGutter: 86, 
+        frameWidth: 427, 
+        srcImg: "./assets/toyota.png", 
+        skewuyContainer: parent3
+    })
+    // const skewuy3 = new Skewuy(200, 15,86,427,"./assets/toyota.png",parent3);
 
 }
